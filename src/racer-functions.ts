6b20013ee0386717aed68fa01253ec5626075a81
Ren <rpaczkowski@gmail.com>
2022-06-30 17:03:43 -0400

    worked on tests for racer functions

--- src/racer-functions.ts
+++ src/racer-functions.ts
@@ -1,3 +1,28 @@
 import { Racer } from "./racer";
 
-export function findRacersWithEmptyFuel(racers: Racer[]) {}
+export function findRacersWithEmptyFuel(racers: Racer[]) {
+  let filteredRacers = racers.filter((racer) => racer.isFuelEmpty());
+  return filteredRacers;
+}
+
+export function findAverageSpeed(racers: Racer[]) {
+  if (!racers.length) return 0;
+  let sum = 0;
+  for (let racer of racers) {
+    sum += racer.speed;
+  }
+  return sum / racers.length;
+}
+
+export function getFasterRacer(racerA: Racer, racerB: Racer) {
+  if (racerA.speed > racerB.speed) {
+    return racerA;
+  } else if (racerA.speed < racerB.speed) {
+    return racerB;
+  } else if (racerA.speed === racerB.speed) {
+    return null;
+  }
+
+  //   const fasterRacer = racerA.speed > racerB.speed ? racerA : racerB
+  //   return fasterRacer
+}
--- tests/racer-functions.test.ts
+++ tests/racer-functions.test.ts
@@ -1,1 +1,287 @@
-import { findRacersWithEmptyFuel } from "../src/racer-functions";
+import { Racer } from "../src/racer";
+import { GasCar } from "../src/gas-car";
+import { SolarCar } from "../src/solar-car";
+import {
+  findRacersWithEmptyFuel,
+  findAverageSpeed,
+  getFasterRacer,
+} from "../src/racer-functions";
+
+describe("findRacersWithEmptyFuel", () => {
+  test("Return an array with two empty cars that have no fuel", () => {
+    const racers: GasCar[] = [
+      {
+        team: "Fire Tires",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Smokin' Wheels",
+        speed: 75,
+        fuel: 15,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return false;
+        },
+      },
+      {
+        team: "Chickens",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+    ];
+
+    const emptyRacers = findRacersWithEmptyFuel(racers);
+
+    expect(emptyRacers).toContain([
+      {
+        team: "Fire Tires",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Chickens",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+    ]);
+  });
+  test("Return an array with all three cars", () => {
+    const racers: GasCar[] = [
+      {
+        team: "Fire Tires",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Smokin' Wheels",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Chickens",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+    ];
+
+    const emptyRacers = findRacersWithEmptyFuel(racers);
+
+    expect(emptyRacers).toContain([
+      {
+        team: "Fire Tires",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Smokin' Wheels",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Chickens",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+    ]);
+  });
+});
+
+describe("findAverageSpeed", () => {
+  test("The average speed of the cars is 65", () => {
+    const racers: GasCar[] = [
+      {
+        team: "Fire Tires",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Smokin' Wheels",
+        speed: 85,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Chickens",
+        speed: 35,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+    ];
+
+    const averageSpeed = findAverageSpeed(racers);
+
+    expect(averageSpeed).toEqual(65);
+  });
+  test("The average speed of gas and solar cars is ___", () => {
+    const racers: GasCar[] = [
+      {
+        team: "Fire Tires",
+        speed: 75,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Smokin' Wheels",
+        speed: 85,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Chickens",
+        speed: 35,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+    ];
+
+    const averageSpeed = findAverageSpeed(racers);
+
+    expect(averageSpeed).toEqual(65);
+  });
+  test("The average speed of the cars is 0", () => {
+    const racers: GasCar[] = [
+      {
+        team: "Fire Tires",
+        speed: 0,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Smokin' Wheels",
+        speed: 0,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+      {
+        team: "Chickens",
+        speed: 0,
+        fuel: 0,
+        accelerate() {
+          return this.speed;
+        },
+        isFuelEmpty() {
+          return true;
+        },
+      },
+    ];
+
+    const averageSpeed = findAverageSpeed(racers);
+
+    expect(averageSpeed).toEqual(0);
+  });
+  test("The average speed of an empty array is 0", () => {
+    const racers: Racer[] = [];
+
+    const averageSpeed = findAverageSpeed(racers);
+
+    expect(averageSpeed).toEqual(65);
+  });
+});
+
+describe("getFasterRacer", () => {
+  test("RacerA is faster", () => {});
+});
